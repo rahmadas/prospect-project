6e7d3e3cb5af7a7848422e8918d4fea89567d915
rahmadas <rahmadadhisasono@gmail.com>
2023-10-05 17:06:28 +0800

    add filed latitude and longtitude in events

---
+++ database/migrations/2023_10_05_122154_add_latitude_in_events.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->string('latitude')->after('location');
+            $table->string('longitude')->after('latitude');
+            // $table->string('location')->nullable()->after('latitude');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            // $table->dropColumn('latitude');
+            // $table->dropColumn('longitude');
+            // $table->dropColumn('location')->nullable()->after('longitude');
+            //Periksa apakah kolom 'location' memiliki atribut nullable
+            // if (Schema::hasColumn('events', 'location')) {
+            //     $table->dropColumn('location')->nullable()->change();
+            // } else {
+            //     // Jika kolom 'location' tidak ada, Anda bisa menambahkannya dengan nullable
+            //     $table->dropColumn('location')->nullable();
+            // }
+        });
+    }
+};
