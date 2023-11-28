89d6f4008fd2fcf2dccd65501c3605fcb3a71e4b
rahmadas <rahmadadhisasono@gmail.com>
2023-11-28 11:10:15 +0800

    update in migrations

--- database/migrations/2023_11_28_102538_add_title_in_tutorials.php
+++ database/migrations/2023_11_28_102538_add_title_in_tutorials.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tutorials', function (Blueprint $table) {
-            $table->string('title')->after('id');
+            // $table->string('title')->after('id');
         });
     }
 
--- database/migrations/2023_11_28_103558_change_start_date_in_tasks.php
+++ database/migrations/2023_11_28_103558_change_start_date_in_tasks.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            // $table->date('start_date')->change();
+            $table->date('start_date')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->datetime('start_date')->change();
+            // $table->date('start_date');
         });
     }
 };
