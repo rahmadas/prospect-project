11cda53b425a9e80fc404632fc4f36db02a9c78b
rahmadas <rahmadadhisasono@gmail.com>
2023-12-11 16:24:30 +0800

    lanjutan commit

--- database/migrations/2023_12_08_142716_change_field_in_users.php
+++ database/migrations/2023_12_08_142716_change_field_in_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('foto_profile')->after('status')->nullable();
+            // $table->string('foto_profile')->after('status')->nullable();
         });
     }
 
